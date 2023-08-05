6828221bdf99cb73292436849ab8f47eaa1f3f67
RISHU RAJ <108472688+rishabh928y@users.noreply.github.com>
2023-08-05 12:42:57 +0530

    Added solution - LeetHub

--- M-Coloring Problem - GFG/mcoloring-problem.cpp
+++ M-Coloring Problem - GFG/mcoloring-problem.cpp
@@ -8,56 +8,44 @@ class Solution{
 public:
     // Function to determine if graph can be coloured with at most M colours such
     // that no two adjacent vertices of graph are coloured with same colour.
-    
-    bool issafe(int idx, bool graph[101][101], int color[], int c, int n) {
-         
-         for(int i=0; i<n; i++) {
-             if(graph[i][idx] && c == color[i]) {
-                 return false;
-             }
-         }
-         
-         return true;
+    bool issafe(bool graph[101][101], int c, vector<int>& color, int idx, int n) {
+        
+        for(int i=0; i<n; i++) {
+            if(graph[i][idx] && c == color[i]) {
+                return false;
+            }
+        }
+        
+        return true;
     }
     
-    
-    bool solve(bool graph[101][101], int m, int color[], int n, int idx) {
-          
+    bool solve(bool graph[101][101], int m, int n, int idx, vector<int>& color) {
+        
          if(idx == n) {
              return true;
-         } 
+         }
          
          for(int c=1; c<=m; c++) {
-             if(issafe(idx, graph, color, c, n)) {
+             if(issafe(graph, c, color, idx, n)) {
                  color[idx] = c;
-             
-                 if(solve(graph, m, color, n, idx+1)) {
+                 
+                 if(solve(graph, m, n, idx+1, color)) {
                      return true;
                  }
-                 
                  
                  color[idx] = 0;
              }
          }
-         
-         return false;
+        
+        return false;
     }
     
     bool graphColoring(bool graph[101][101], int m, int n) {
         
-         int color[n];
          
-         for(int i=0; i<n; i++) {
-             color[i] = 0;
-         }
-         
-         if(solve(graph, m, color, n, 0)) {
-             return true;
-         }
-         else {
-             return false;
-         }
+        vector<int>color(n, 0);
         
+        return solve(graph, m, n, 0, color);
         
     }
 };
