05f27ec2b37d172fe6124bed7327729917cc7bc3
RISHU RAJ <108472688+rishabh928y@users.noreply.github.com>
2023-09-19 18:55:34 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -5,95 +5,47 @@ using namespace std;
 // } Driver Code Ends
 class Solution
 {
+    typedef pair<int, int> P;
+    
 	public:
-	vector<int> parent;
-	vector<int> rank;
-	
-	int find(int x) {
-	    if(x == parent[x]) {
-	        return x;
-	    }
-	    
-	    return parent[x] = find(parent[x]);
-	}
-	void Union(int x, int y) {
-	    
-	    int x_parent = find(x);
-	    int y_parent = find(y);
-	    
-	    if(x_parent == y_parent) {
-	        return;
-	    }
-	    
-	    if(rank[x_parent] > rank[y_parent]) {
-	        parent[y_parent] = x_parent;
-	    }
-	    else if(rank[x_parent] < rank[y_parent]) {
-	        parent[x_parent] = y_parent;
-	    }
-	    else {
-	        
-	        parent[x_parent] = y_parent;
-	        rank[y_parent]++;
-	        
-	    }
-	    
-	}
-	
-	int kruskal(vector<vector<int>>& vec) {
-	    
-	    int sum = 0;
-	    for(auto &temp : vec) {
-	       
-	        int u  = temp[0];
-	        int v  = temp[1];
-	        int wt = temp[2];
-	        
-	        int parent_u = find(u);
-	        int parent_v = find(v);
-	        
-	        if(parent_u != parent_v) {
-	            Union(u, v);
-	            sum += wt;
-	        }
-	        
-	    }
-	    
-	    return sum;
-	}
-	
+	//Function to find sum of weights of edges of the Minimum Spanning Tree.
     int spanningTree(int V, vector<vector<int>> adj[])
     {
-        parent.resize(V);
-        rank.resize(V);
         
-        for(int i=0; i<V; i++) {
-            parent[i] = i;
-        }
+        priority_queue<P, vector<P>, greater<P>> pq;
+        pq.push({0, 0});
+        vector<bool> inMST(V, false);
         
-        vector<vector<int>> vec;
+        int sum = 0;
         
-        for(int u=0; u<V; u++) {
+        while(!pq.empty()) {
+           
+            auto p = pq.top();
+            pq.pop();
             
-            for(auto &temp : adj[u]) {
-                
-                int v  = temp[0];
-                int wt = temp[1];
-                
-                vec.push_back({u, v, wt});
-                
+            int wt = p.first;
+            int node = p.second;
+            
+            if(inMST[node] == true) {
+                continue;
             }
+            
+            inMST[node] = true;
+            sum += wt;
+            
+            for(auto &temp : adj[node]) {
+                
+                int neighbor = temp[0];
+                int neighbor_wt = temp[1];
+                
+                if(inMST[neighbor] == false) {
+                    pq.push({neighbor_wt, neighbor});
+                }
+            }   
+            
         }
         
-        auto comp = [&](vector<int>& vec1, vector<int>& vec2) {
-          
-             return vec1[2] < vec2[2];
-            
-        };
-        
-        sort(begin(vec), end(vec), comp);
-        
-        return kruskal(vec);
+        return sum;
     }
 };
 
