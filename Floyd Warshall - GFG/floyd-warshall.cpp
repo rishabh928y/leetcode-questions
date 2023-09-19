e8890c7a9304eeb33d1852aa3a22a17c18832486
RISHU RAJ <108472688+rishabh928y@users.noreply.github.com>
2023-09-19 16:43:42 +0530

    Added solution - LeetHub

--- Floyd Warshall - GFG/floyd-warshall.cpp
+++ Floyd Warshall - GFG/floyd-warshall.cpp
@@ -10,43 +10,37 @@ using namespace std;
 class Solution {
   public:
 	void shortest_distance(vector<vector<int>>&grid){
-
-         int n = grid.size();
-         
-         for(int i=0; i<n; i++) {
-             for(int j=0; j<n; j++) {
-                 
-                 if(grid[i][j] == -1) {
-                     grid[i][j] = 10001;
-                 }
-                 
-             }
-         }
-         
-         for(int via = 0; via < n; via++) {
-             
-             for(int i=0; i<n; i++) {
-                 for(int j=0; j<n; j++) {
-                     
-                     grid[i][j] = min(grid[i][j], grid[i][via] + grid[via][j]);
-                     
-                 }
-             }
-             
-             
-         }
-         
-         for(int i=0; i<n; i++) {
-             for(int j=0; j<n; j++) {
-                 
-                 if(grid[i][j] == 10001) {
-                     grid[i][j] = -1;
-                 }
-                 
-             }
-         }
-	    
-	    
+	      
+	     int n = grid.size();
+	     
+	     for(int i=0; i<n; i++) {
+	         for(int j=0; j<n; j++) {
+	             if(grid[i][j] == -1) {
+	                 grid[i][j] = 10001;
+	             }
+	         }
+	     }
+	     
+	     for(int via = 0; via<n; via++) {
+	         
+	         for(int i=0; i<n; i++) {
+	             for(int j=0; j<n; j++) {
+	                 
+	                 grid[i][j] = min(grid[i][j], grid[i][via] + grid[via][j]);
+	                 
+	             }
+	         }
+	         
+	     }
+	     
+	     for(int i=0; i<n; i++) {
+	         for(int j=0; j<n; j++) {
+	             if(grid[i][j] == 10001) {
+	                 grid[i][j] = -1;
+	             }
+	         }
+	     }
+	      
 	}
 };
 
